7c8aa8dd1338c219080b8cb1d0c9059f3921f15a
Nate Silva <nate@natesilva.com>
2017-02-16 09:42:22 -0800

    update to 3.0.1

--- Support/assets/document.js
+++ Support/assets/document.js
@@ -3,7 +3,7 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function() {
-  var VERSION = '3.0.0';
+  var VERSION = '3.0.1';
 
 	// parse a version number into semver parts
   var parseVersion = function(ver) {
